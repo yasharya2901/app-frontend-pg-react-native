75b929402cde2d75243bc2de4abfe3b228d95d80
yasharya2901 <yasharya2901@gmail.com>
2024-07-03 00:03:06 +0530

    Configured Icons for Global Access

---
+++ constants/icons.js
@@ -0,0 +1,17 @@
+import account from "../assets/icons/png/account.png"
+import back from "../assets/icons/png/back.png"
+import home from "../assets/icons/png/home.png"
+import manage from "../assets/icons/png/manage.png"
+import menu from "../assets/icons/png/menu.png"
+import notification from "../assets/icons/png/notification.png"
+import scan_camera from '../assets/icons/png/scan_camera.png'
+
+export default {
+    account,
+    back,
+    home,
+    manage,
+    menu,
+    notification,
+    scan_camera
+};
