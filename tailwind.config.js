bbfad5ca54e92b28b9bf488cb0fb997acbf053ec
yasharya2901 <yasharya2901@gmail.com>
2024-07-03 00:25:13 +0530

    Color Palette Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F7a767",
+        secondary: "#F4F2F0",
+        black: {
+          DEFAULT: "#000",
+          100: "#2B2826"
+        },
+        disabled: "#757372"
+      }
+    },
   },
   plugins: [],
 }
