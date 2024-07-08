89be1dddb4129a2d15b29bd2068654d866772793
yasharya2901 <yasharya2901@gmail.com>
2024-07-08 15:51:53 +0530

    Sign In Page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,23 +1,75 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Animated } from 'react-native'
+import React, { useState, useRef } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CustomButton from '../../components/CustomButton'
+import LoginField from '../../components/LoginField'
 
 const SignIn = () => {
+  const [usePassword, setUsePassword] = useState(true);
+  const [form, setForm] = useState({
+    mobile: '',
+    password: '',
+    otp:'',
+    usePassword: usePassword
+  })
+
+  const opacity = useRef(new Animated.Value(1)).current;
+
+  const toggleUsePassword = () => {
+      setUsePassword(!usePassword);
+      setForm({...form, usePassword: !usePassword})
+  };
+
   return (
-    <SafeAreaView className="flex flex-col">
-        <View>
-        <Text>SignIn</Text>
+    <SafeAreaView className="flex flex-col p-4 h-full justify-between bg-white">
+      <View className="">
+        <View className="h-40 flex justify-end items-center">
+          <Text className={`font-pbold text-2xl w-max`}>Sign In</Text>
 
         </View>
+        <View className="flex justify-around h-80 mt-12">
+          <View>
+            <Text className={`font-pbold text-left text-2xl`}>Welcome Back!</Text>
+            <Text className={`font-pmedium text-base text-left`}>Log in to your account to enjoy the seamless service of Uniworld.</Text>
+          </View>
+          <View>
+            <LoginField
+              placeholder="Mobile number"
+              value={form.mobile}
+              handleChangeText={(e) => setForm({...form, mobile: e})}
+              keyboardType='phone-pad'
+            />
+
+            {(usePassword) ? 
+                  <LoginField
+                    placeholder="Password"
+                    value={form.password}
+                    handleChangeText={(e) => setForm({...form, password: e})}
+                    secureTextEntry={true}
+                  /> :
+                  <LoginField
+                    placeholder="OTP"
+                    value={form.opt}
+                    handleChangeText={(e) => setForm({...form, otp: e})}
+                    keyboardType='number-pad'
+                  />
+            }
+            <TouchableOpacity activeOpacity={0.8} onPress={toggleUsePassword}>
+              <Text className={`font-pmedium text-sm text-right`}>{(usePassword) ? "Login Using OTP" : "Login Using Password"}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View className={`h-14`}>
         <View className="flex flex-row">
             <View className="flex-1">
-                <CustomButton buttonStyle="bg-primary rounded-md" textStyle={`font-pmedium`} text='Reset Password' handlePress={()=>console.log("Reset Password")}/>
+                <CustomButton buttonStyle="bg-secondary rounded-md mr-2" textStyle={`font-pbold text-base`} text='Reset Password' handlePress={()=>console.log("Reset Password")}/>
             </View>
             <View className="flex-1">
-                <CustomButton buttonStyle="bg-primary rounded-md" textStyle={`font-pmedium`} text='Sign In' handlePress={()=>console.log("Sign In")}/>
+                <CustomButton buttonStyle="bg-primary rounded-md ml-2" textStyle={`font-pbold text-base`} text='Sign In' handlePress={()=>console.log(form)}/>
             </View>
         </View>
+      </View>
     </SafeAreaView>
   )
 }
