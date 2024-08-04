0365e523b1753af18ce049a348c37296dffd4ec7
Yash Arya <yasharya2901@gmail.com>
2024-08-04 19:27:20 +0530

    Revert "Edited MealCard component to functioning component"
    This reverts commit 94f46f7ed9bc2c32586f951eb924689adc1b8ea9.
    Aditya's merge didn't work with current build.

--- components/CustomButton/CustomButton.jsx
+++ components/CustomButton/CustomButton.jsx
@@ -1,9 +1,9 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CustomButton = ({buttonStyle, handlePress, textStyle, text="Button", disabled = false}) => {
+const CustomButton = ({buttonStyle, handlePress, textStyle, text="Button"}) => {
   return (
-    <TouchableOpacity className={`${buttonStyle} p-2`} onPress={handlePress} activeOpacity={0.7} disabled={disabled}>
+    <TouchableOpacity className={`${buttonStyle} p-2`} onPress={handlePress} activeOpacity={0.7}>
         <Text className={`text-center ${textStyle}`}>{text}</Text>
     </TouchableOpacity>
   )
