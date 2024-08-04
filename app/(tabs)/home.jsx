8001032a57df4733ab283f04fa5193659c240cb3
Yash Arya <yasharya2901@gmail.com>
2024-08-04 21:39:47 +0530

    Revert "Merge branch 'main' into managePage"
    This reverts commit ebb849e871132ed086307b204771264b4afa0b18, reversing
    changes made to 1e0f3de79b8bb0bb1d05d24e3cdcd3ae13f225eb.

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,4 +1,4 @@
-import { Dimensions, FlatList, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MealCard from "../../components/MealCard/MealCard";
@@ -6,55 +6,11 @@ import { svgs } from "../../constants";
 import RightArrow from "../../assets/icons/svg/rightArrow.svg";
 import { getTodayMenu } from "../../api/menu/menu";
 import { router } from "expo-router";
-import Carousel from "react-native-reanimated-carousel";
-import CustomButton from "../../components/CustomButton";
 
 const Home = () => {
   const name = "Aditya";
-  const [width, setWidth] = useState(Dimensions.get("window").width);
-  const [defaultIndex, setDefaultIndex] = useState(0); // default index of meal card carousel
-  
-  
+
   const [weekMenu, setWeekMenu] = useState([]);
-  const [error, setError] = useState(null);
-
-  const getDefaultIndex = (weekMenu) => {
-    
-    const meals = weekMenu?.meals;
-    const currentTime = new Date().getTime();
-    // convert current time to 24 hour format
-    const currentHour = new Date(currentTime).getHours();
-    const currentMinute = new Date(currentTime).getMinutes();
-    
-    let defaultIndex = 0;
-
-    if (!meals) {
-      return defaultIndex;
-    }
-
-    for (let i = 0; i < meals.length; i++) {
-      const meal = meals[i];
-      const mealStartTime = meal.startTime;
-      const mealEndTime = meal.endTime;
-      const mealStartHour = Number(mealStartTime.split(" ")[1].split(":")[0]);
-      const mealStartMinute = Number(mealStartTime.split(" ")[1].split(":")[1]);
-      const mealEndHour = Number(mealEndTime.split(" ")[1].split(":")[0]);
-      const mealEndMinute = Number(mealEndTime.split(" ")[1].split(":")[1]);
-      if (currentHour < mealStartHour && currentHour < mealEndHour) {
-        defaultIndex = i;
-        break;
-      } else if(currentHour >= mealStartHour && currentHour <= mealEndHour) {
-        defaultIndex = i;
-        break;
-      } else if (currentHour === mealStartHour) {
-        if (currentMinute <= mealStartMinute && currentMinute >= mealEndMinute) {
-          defaultIndex = i;
-          break;
-        }
-      }
-    }
-    return defaultIndex;
-  }
   const renderItem = (mealType, startTime, endTime) => {
     return (
       <View className="mb-[120px]">
@@ -63,21 +19,6 @@ const Home = () => {
     );
   };
 
-  const convertTimeStampToTime = (timeStamp) => {
-    timeStamp = timeStamp.split(" ")[1].slice(0, 5);
-    timeStamp = timeStamp.split(":");
-    let amOrPm = "";
-    if (Number(timeStamp[0]) > 12) {
-      timeStamp[0] -= 12;
-      amOrPm = "PM";
-    } else if (Number(timeStamp[0]) === 12) {
-      amOrPm = "PM";
-    } else {
-      amOrPm = "AM";
-    }
-    timeStamp = `${timeStamp[0]}:${timeStamp[1]} ${amOrPm}`;
-    return timeStamp;
-  }
   const ItemSeparator = () => <View />;
 
   useEffect(() => {
@@ -85,27 +26,12 @@ const Home = () => {
       try {
         const response = await getTodayMenu();
         setWeekMenu(response.menuToday[0]);
-        setDefaultIndex(getDefaultIndex(response.menuToday[0]));
-        setError(null);
       } catch (error) {
-        setError(error);
         console.error(error);
       }
     };
     fetchMenu();
   }, []);
-
-  const handleRetry = async () => {
-    try {
-      const response = await getTodayMenu();
-      setWeekMenu(response.menuToday[0]);
-      setDefaultIndex(getDefaultIndex(response.menuToday[0]));
-      setError(null);
-    } catch (error) {
-      setError(error);
-      console.error(error);
-    }
-  }
 
   return (
     <SafeAreaView className="flex flex-col p-4 h-full justify-between bg-white">
@@ -118,7 +44,7 @@ const Home = () => {
       <View className="mb-10">
         <Text className="font-bold text-2xl ">{`Good Evening, ${name}!`}</Text>
       </View>
-      {/* <FlatList
+      <FlatList
         data={weekMenu?.meals}
         renderItem={({ item }) => {
           return renderItem(item.name, item.startTime, item.endTime);
@@ -128,17 +54,7 @@ const Home = () => {
         showsHorizontalScrollIndicator={false}
         pagingEnabled
         ItemSeparatorComponent={ItemSeparator}
-      /> */}
-      {(weekMenu.meals) ? (<Carousel
-        width={width - 32}
-        autoPlay={false}
-        data={weekMenu?.meals}
-        defaultIndex={defaultIndex}
-        renderItem={({item}) => (renderItem(item.name, item.startTime, item.endTime))}
-      />): (error) ? (<View className={`h-full flex items-center justify-center`}>
-        <Text className={`font-pbold text-lg rounded-lg shadow-2xl`}>Oops! Something Went Wrong...</Text>
-        <CustomButton text="Retry" handlePress={handleRetry} textStyle={``}/>
-      </View>) : (<Text className="font-pregular text-center mb-[500px]">No meals for today</Text>)}
+      />
       <View>
         <View className="flex items-end">
           <TouchableOpacity
